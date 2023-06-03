540e50a4bd1c695c38253125e40cfe46d5ed2875
Muhammad Owais <rahneverd@gmail.com>
2023-06-03 21:07:55 +0500

    uploads made public

--- BE/app.js
+++ BE/app.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(express.static('uploads'));
 
 app.use('/', require('./router'));
 
